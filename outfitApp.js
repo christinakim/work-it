1d18eb54b82ee4b74cd1df595a436a9b23e1e539
christinakim <christinahkim1@gmail.com>
2014-10-13 03:41:13 -0500

    added confirm

--- outfitApp.js
+++ outfitApp.js
@@ -1,1 +1,19 @@
-var app = angular.module("outfitApp", []);
+'use strict';
+
+var outfitApp = angular.module("outfitApp", ['ngRoute', 'outfitControllers'];
+
+
+outfitApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/confirm', {
+        templateUrl: 'views/confirm.html',
+        controller: 'FormCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
--- outfitCtrl.js
+++ outfitCtrl.js
@@ -1,3 +1,6 @@
+var outfitControllers = angular.module('outfitControllers', []);
+
+
 app.controller("outfitCtrl", function($scope) {
     $scope.message = "";
     $scope.clear = function() {$scope.message = "";};
